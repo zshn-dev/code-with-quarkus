a29f86209549d356f00b60d830b6d80193639b26
Zeeshan Siddiqui <mzees88@gmail.com>
2024-02-26 14:28:21 -0800

    Run docker container in detached mode

--- deploy.cmd
+++ deploy.cmd
@@ -1,2 +1,2 @@
 docker pull quarkus/code-with-quarkus-jvm:latest
-start docker run -i --rm -p 8080:8080 --name code-with-quarkus-jvm quarkus/code-with-quarkus-jvm & exit 0
+docker run -i -d --rm -p 8080:8080 --name code-with-quarkus-jvm quarkus/code-with-quarkus-jvm
