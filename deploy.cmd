ed86a63d8c40c8b12274512fbf6f0d034b836fe5
Zeeshan Siddiqui <mzees88@gmail.com>
2024-02-26 14:15:55 -0800

    Updated deployment

--- deploy.cmd
+++ deploy.cmd
@@ -1,19 +1,2 @@
 docker pull quarkus/code-with-quarkus-jvm:latest
-
-set "CONTAINER_NAME=code-with-quarkus"
-docker ps -qa -f name=!CONTAINER_NAME! > nul && (
-    docker ps -q -f name=!CONTAINER_NAME! > nul && (
-        echo Container is running -^> stopping it...
-        docker stop !CONTAINER_NAME!
-    )
-)
-
-docker run -i --rm -p 8080:8080 quarkus/code-with-quarkus-jvm
-
-exit
-
-if %ERRORLEVEL% EQU 0 (
-    exit /b 0
-) else (
-    exit /b 1
-)
+start docker run -i --rm -p 8080:8080 --name code-with-quarkus-jvm quarkus/code-with-quarkus-jvm & exit 0
